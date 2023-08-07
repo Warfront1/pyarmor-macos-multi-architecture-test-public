28280b3246a887743e9f1eaae51e2a1ac6599264
Warfront1 <1920038+Warfront1@users.noreply.github.com>
2023-08-07 15:59:36 -0400

    Remove venv

--- build.sh
+++ build.sh
@@ -1,43 +1,34 @@
 #!/bin/zsh
 #set -e
-rm -rf ./build ./dist ./.pyarmor ./test.spec ~/pyarmortesting
-cwd=$(pwd)
-python3 -m venv ~/pyarmortesting/
-cd ~/pyarmortesting/
-source ./bin/activate
-cd $cwd
-pip install pyarmor==8.3.1 pyinstaller==5.11.0 pyarmor.cli.core.darwin==4.3.1
-pyinstaller --target-architecture universal2 test.py
+rm -rf ./build ./dist ./.pyarmor
+pip install pyarmor==8.3.1 pyarmor.cli.core.darwin==4.3.1
 sed -i '' '248a\
         if not os.path.exists(rtpath):
-'  ~/pyarmortesting/lib/python3.11/site-packages/pyarmor/cli/plugin.py
+'  /Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pyarmor/cli/plugin.py
 sed -i '' '249a\
             print("We are inside of the patch ", ctx.repack_path, ctx.runtime_package_name)
-'  ~/pyarmortesting/lib/python3.11/site-packages/pyarmor/cli/plugin.py
+'  /Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pyarmor/cli/plugin.py
 sed -i '' '250a\
             rtpath = os.path.join(ctx.repack_path, ctx.runtime_package_name)
-'  ~/pyarmortesting/lib/python3.11/site-packages/pyarmor/cli/plugin.py
+'  /Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pyarmor/cli/plugin.py
 sed -i '' '251a\
         print("We are right after the patch")
-'  ~/pyarmortesting/lib/python3.11/site-packages/pyarmor/cli/plugin.py
+'  /Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pyarmor/cli/plugin.py
 sed -i '' '273a\
 print("Patching sed commands are working")
-'  ~/pyarmortesting/lib/python3.11/site-packages/pyarmor/cli/plugin.py
-sed -n '235,255p' ~/pyarmortesting/lib/python3.11/site-packages/pyarmor/cli/plugin.py
-pyarmor gen --platform darwin.x86_64,darwin.arm64 --pack dist/test/test test.py
-if [ $? -ne 0 ]; then
-  echo "Command failed with non-zero exit code."
-  if [ -f .pyarmor/pack/dist/pyarmor_runtime_000000/darwin_arm64/pyarmor_runtime.so ]; then
-        echo "darwin_arm64 pyarmor_runtime.so exists!"
-  else
-        echo "darwin_arm64 pyarmor_runtime.so does not exist."
-  fi
-  if [ -f .pyarmor/pack/dist/pyarmor_runtime_000000/darwin_x86_64/pyarmor_runtime.so ]; then
-        echo "darwin_x86_64pyarmor_runtime.so exists!"
-  else
-        echo "darwin_x86_64 pyarmor_runtime.so does not exist."
-  fi
+'  /Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pyarmor/cli/plugin.py
+sed -n '235,255p' /Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pyarmor/cli/plugin.py
+#find / -type d -name "cli" -exec find {} -type f -name "plugin.py" \;
+python -m pyarmor.cli gen --platform darwin.x86_64,darwin.arm64 test.py
+if [ -f .pyarmor/pack/dist/pyarmor_runtime_000000/darwin_arm64/pyarmor_runtime.so ]; then
+      echo "darwin_arm64 pyarmor_runtime.so exists!"
 else
-  dist/test/test
+      echo "darwin_arm64 pyarmor_runtime.so does not exist."
 fi
+if [ -f .pyarmor/pack/dist/pyarmor_runtime_000000/darwin_x86_64/pyarmor_runtime.so ]; then
+      echo "darwin_x86_64pyarmor_runtime.so exists!"
+else
+      echo "darwin_x86_64 pyarmor_runtime.so does not exist."
+fi
+ls -a dist/pyarmor_runtime_000000
 pyarmor cfg plugins
